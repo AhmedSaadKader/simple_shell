cd49403aa88b73e55688297f7c0152822893cd7f
moharafat <mohamedamr0001@gmail.com>
2023-12-13 19:35:22 +0200

    127

--- which_x.c
+++ which_x.c
@@ -9,18 +9,15 @@
 
 char **which_x(char **av)
 {
-	char *path, *path_copy = NULL, *delimiter,
-	*token, *pathname = NULL, *filename;
+	char *path_copy = NULL, *token, *pathname = NULL, *filename;
 	struct stat st;
 	size_t path_len, filename_len, total_len;
 
-	filename = malloc(sizeof(char) * (strlen(av[0]) + 3));
+	filename = malloc(sizeof(char) * (strlen(av[0]) + 2));
 	strcpy(filename, "/");
 	strcat(filename, av[0]);
-	delimiter = ":";
-	path = getenv("PATH");
-	path_copy = strdup(path);
-	token = strtok(path_copy, delimiter);
+	path_copy = strdup(getenv("PATH"));
+	token = strtok(path_copy, ":");
 	while (token != NULL)
 	{
 		path_len = strlen(token);
@@ -40,10 +37,11 @@ char **which_x(char **av)
 			free(av[0]);
 			av[0] = pathname;
 			free(path_copy);
+			free(filename);
 			return (av);
 		}
 		free(pathname);
-		token = strtok(NULL, delimiter);
+		token = strtok(NULL, ":");
 	}
 	free(path_copy);
 	free(filename);
